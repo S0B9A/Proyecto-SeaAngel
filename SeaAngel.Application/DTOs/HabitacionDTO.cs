9ec30439456b83f74c3cc8643ac00f0cd9dd1789
sebas <sebas@DESKTOP-I0KMSBS>
2025-02-24 13:20:36 -0600

    Se agrega el merge combinando la rama fiorella y sebas

--- SeaAngel.Application/DTOs/HabitacionDTO.cs
+++ SeaAngel.Application/DTOs/HabitacionDTO.cs
@@ -7,11 +7,20 @@ using SeaAngel.Infraestructure.Models;
 
 namespace SeaAngel.Application.DTOs
 {
+    public class HabitacionDTO
     {
+        public int ID { get; set; }
 
         public string Nombre { get; set; } = null!;
 
+        public string? Descripcion { get; set; }
 
+        public int CapacidadMin { get; set; }
 
+        public int CapacidadMax { get; set; }
+
+        public decimal TamanoM2 { get; set; }
+
+        public virtual List<BarcoHabitacion> BarcoHabitacion { get; set; } = null!;
     }
 }
