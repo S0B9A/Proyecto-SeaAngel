aee4ce6e2ea1b14f989e4974005f88fdbf3688e7
Fiorella <fiotc284@gmail.com>
2025-03-26 18:09:46 -0600

    Login

---
+++ SeaAngel.Application/Services/Implementations/ServiceUsuario.cs
@@ -0,0 +1,99 @@
+﻿using AutoMapper;
+using Libreria.Application.Config;
+using Libreria.Application.Utils;
+using Microsoft.Extensions.Options;
+using SeaAngel.Application.DTOs;
+using SeaAngel.Application.Services.Interfaces;
+using SeaAngel.Infraestructure.Models;
+using SeaAngel.Infraestructure.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaAngel.Application.Services.Implementations
+{
+    public class ServiceUsuario : IServiceUsuario
+    {
+        private readonly IRepositoryUsuario _repository;
+        private readonly IMapper _mapper;
+        private readonly IOptions<AppConfig> _options;
+
+        public ServiceUsuario(IRepositoryUsuario repository, IMapper mapper, IOptions<AppConfig> options)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _options = options;
+        }
+
+        public async Task<string> AddAsync(UsuarioDTO dto)
+        {
+            // Llave secreta
+            string secret = _options.Value.Crypto.Secret;
+            // Password encriptado
+            string passwordEncrypted = Cryptography.Encrypt(dto.Contraseña!, secret);
+            // Establecer password DTO
+            dto.Contraseña = passwordEncrypted;
+            var objectMapped = _mapper.Map<Usuario>(dto);
+
+            return await _repository.AddAsync(objectMapped);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+
+
+        public async Task<ICollection<UsuarioDTO>> FindByDescriptionAsync(string description)
+        {
+            var list = await _repository.FindByDescriptionAsync(description);
+            var collection = _mapper.Map<ICollection<UsuarioDTO>>(list);
+            return collection;
+        }
+
+        public async Task<UsuarioDTO> FindByIdAsync(string id)
+        {
+            var @object = await _repository.FindByIdAsync(id);
+            var objectMapped = _mapper.Map<UsuarioDTO>(@object);
+            return objectMapped;
+        }
+
+
+        public async Task<ICollection<UsuarioDTO>> ListAsync()
+        {
+            var list = await _repository.ListAsync();
+            var collection = _mapper.Map<ICollection<UsuarioDTO>>(list);
+            // Return Data
+            return collection;
+        }
+
+        public async Task<UsuarioDTO> LoginAsync(string id, string password)
+        {
+            UsuarioDTO usuarioDTO = null!;
+
+            // Llave secreta
+            string secret = _options.Value.Crypto.Secret;
+            // Password encriptado
+            string passwordEncrypted = Cryptography.Encrypt(password, secret);
+
+            var @object = await _repository.LoginAsync(id, passwordEncrypted);
+
+            if (@object != null)
+            {
+                usuarioDTO = _mapper.Map<UsuarioDTO>(@object);
+            }
+
+            return usuarioDTO;
+        }
+
+        public async Task UpdateAsync(string id, UsuarioDTO dto)
+        {
+            var @object = await _repository.FindByIdAsync(id);
+            //       source, destination
+            _mapper.Map(dto, @object!);
+            await _repository.UpdateAsync();
+        }
+    }
+}
---
+++ SeaAngel.Infraestructure/Repository/Implementations/RepositoryUsuario.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SeaAngel.Infraestructure.Data;
+using SeaAngel.Infraestructure.Models;
+using SeaAngel.Infraestructure.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaAngel.Infraestructure.Repository.Implementations
+{
+    public class RepositoryUsuario : IRepositoryUsuario
+    {
+        private readonly SeanAngelContext _context;
+
+        public RepositoryUsuario(SeanAngelContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<string> AddAsync(Usuario entity)
+        {
+            await _context.Set<Usuario>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity.CorreoElectronico;
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+
+            var @object = await FindByIdAsync(id);
+            _context.Remove(@object);
+            _context.SaveChanges();
+        }
+
+        public async Task<ICollection<Usuario>> FindByDescriptionAsync(string description)
+        {
+            var collection = await _context
+                                         .Set<Usuario>()
+                                         .Where(p => p.Nombre.Contains(description))
+                                         .ToListAsync();
+            return collection;
+        }
+
+        public async Task<Usuario> FindByIdAsync(string id)
+        {
+            var @object = await _context.Set<Usuario>().FindAsync(id);
+
+            return @object!;
+        }
+
+        public async Task<ICollection<Usuario>> ListAsync()
+        {
+            var collection = await _context.Set<Usuario>()
+                                          .ToListAsync();
+            return collection;
+        }
+
+        public async Task<Usuario> LoginAsync(string id, string password)
+        {
+            var @object = await _context.Set<Usuario>()
+                                        .Where(p => p.CorreoElectronico == id && p.Contraseña == password)
+                                        .FirstOrDefaultAsync();
+            return @object!;
+        }
+
+        public async Task UpdateAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
